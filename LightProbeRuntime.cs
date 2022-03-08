3cc5e5ab81b3e1a994dc06b27bca6ac49398670e
Ayfel <alexcastedo@gmail.com>
2022-03-08 10:18:33 -0600

    Added Lightprobe runtime calculation
    Runtime calculation of probes. Prefabs that are lightmapped should include lights as part of their setup, and lightprobes should exists in the scene you instantiate stuff at

---
+++ LightProbeRuntime.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LightProbeRuntime : MonoBehaviour
+{
+    public Color m_Ambient;
+    Light[] m_Lights;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return null;
+
+        m_Lights = FindObjectsOfType<Light>();
+        SphericalHarmonicsL2[] bakedProbes = LightmapSettings.lightProbes.bakedProbes;
+        Vector3[] probePositions = LightmapSettings.lightProbes.positions;
+        int probeCount = LightmapSettings.lightProbes.count;
+
+        // Clear all probes
+        for (int i = 0; i < probeCount; i++)
+            bakedProbes[i].Clear();
+
+        // Add ambient light to all probes
+        for (int i = 0; i < probeCount; i++)
+            bakedProbes[i].AddAmbientLight(m_Ambient);
+
+        // Add directional and point lights' contribution to all probes
+        foreach (Light l in m_Lights)
+        {
+            if (l.type == LightType.Directional)
+            {
+                for (int i = 0; i < probeCount; i++)
+                    bakedProbes[i].AddDirectionalLight(-l.transform.forward, l.color, l.intensity);
+            }
+            else if (l.type == LightType.Point)
+            {
+                for (int i = 0; i < probeCount; i++)
+                    SHAddPointLight(probePositions[i], l.transform.position, l.range, l.color, l.intensity, ref bakedProbes[i]);
+            }
+        }
+        LightmapSettings.lightProbes.bakedProbes = bakedProbes;
+    }
+
+    void SHAddPointLight(Vector3 probePosition, Vector3 position, float range, Color color, float intensity, ref SphericalHarmonicsL2 sh)
+    {
+        // From the point of view of an SH probe, point light looks no different than a directional light,
+        // just attenuated and coming from the right direction.
+        Vector3 probeToLight = position - probePosition;
+        float attenuation = 1.0F / (1.0F + 25.0F * probeToLight.sqrMagnitude / (range * range));
+        sh.AddDirectionalLight(probeToLight.normalized, color, intensity * attenuation);
+    }
+}
